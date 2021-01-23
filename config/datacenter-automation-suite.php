000018c116c3acc90bcbb27ee89011f9ea3aaee7
Alex Renner <alexrenner@webserver.elementalfusion.online>
2021-01-23 00:22:44 +0000

    cypress testing suite install

--- config/datacenter-automation-suite.php
+++ config/datacenter-automation-suite.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-
+    'company_name' => env('COMPANY_NAME'),
+    'domain'       => env('DOMAIN'),
+    'url'          => [
+        'customer'    => env('LOGIN_URL'),
+        'internal'    => env('INTERNAL_LOGIN_URL'),
+        'vendor'      => env('VENDOR_LOGIN_URL'),
+        'whitegloves' => env('WHITEGLOVES_LOGIN_URL'),
+    ],
 ];
+
